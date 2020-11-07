1678aaadc4647ce35f7bfce23406a14d6e864060
Calvin Moody <calvinmcm@gmail.com>
2020-11-07 11:56:58 -0500

    add loader

--- src/components/RecordTable.js
+++ src/components/RecordTable.js
@@ -9,23 +9,35 @@ import {
 	TableRow,
 	Paper,
 } from "@material-ui/core"
+import { Skeleton } from "@material-ui/lab"
 import { Delete as DeleteIcon } from "@material-ui/icons"
 import { getRecords, deleteRecord, getSum } from "../services/record"
 import RecordContext from "../state/record/context"
 import { addRecord, deleteRecord as deleteRecordAction } from "../state/record/actions"
 
+const skeletonStyle = { height: 100 }
+
 const RecordTable = () => {
 	const [rows, setRows] = useState([])
 	const [recordSum, setRecordSum] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [calcLoading, setCalcLoading] = useState(false)
 	const [recordState, recordDispatch] = useContext(RecordContext)
 
 	useEffect(() => {
 		const getRows = async () => {
-			const records = await getRecords()
-			// Add records to context
-			for (let el of records) {
-				recordDispatch(addRecord(el))
+			setLoading(true)
+			try {
+				const records = await getRecords()
+				// Add records to context
+				for (let el of records) {
+					recordDispatch(addRecord(el))
+				}
+			} catch (error) {
+				console.error(error)
+				// TODO
 			}
+			setLoading(false)
 		}
 
 		getRows()
@@ -33,8 +45,15 @@ const RecordTable = () => {
 
 	useEffect(() => {
 		const fetchSum = async () => {
-			const { sum } = await getSum()
-			setRecordSum(sum)
+			setCalcLoading(true)
+			try {
+				const { sum } = await getSum()
+				setRecordSum(sum)
+			} catch (error) {
+				console.error(error)
+				// TODO
+			}
+			setCalcLoading(false)
 		}
 
 		fetchSum()
@@ -52,54 +71,69 @@ const RecordTable = () => {
 
 	return (
 		<TableContainer component={Paper}>
-			{rows.length > 0 ? (
-				<>
-					<Table>
-						<TableHead>
-							<TableRow>
-								<TableCell>
-									<b>Type</b>
-								</TableCell>
-								<TableCell>
-									<b>Name</b>
-								</TableCell>
-								<TableCell>
-									<b>Balance</b>
-								</TableCell>
-								<TableCell>
-									<b>Actions</b>
-								</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{rows.map((row) => (
-								<TableRow key={row.id}>
-									<TableCell>{row.type}</TableCell>
-									<TableCell>{row.name}</TableCell>
-									<TableCell>{row.balance}</TableCell>
-									<TableCell>
-										<IconButton
-											data-testid={`delete-${row.id}`}
-											onClick={() => {
-												handleDelete(row.id)
-											}}
-										>
-											<DeleteIcon />
-										</IconButton>
-									</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-					<TableRow>
-						<TableCell>Total</TableCell>
-						<TableCell>{recordSum}</TableCell>
-					</TableRow>
-				</>
+			{loading ? (
+				<div style={{ padding: "0 16px" }}>
+					<Skeleton style={skeletonStyle} />
+					<Skeleton style={skeletonStyle} />
+					<Skeleton style={skeletonStyle} />
+				</div>
 			) : (
-				<p>
-					<em>Create a record to get started!</em>
-				</p>
+				<>
+					{rows.length > 0 ? (
+						<>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell>
+											<b>Type</b>
+										</TableCell>
+										<TableCell>
+											<b>Name</b>
+										</TableCell>
+										<TableCell>
+											<b>Balance</b>
+										</TableCell>
+										<TableCell>
+											<b>Actions</b>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{rows.map((row) => (
+										<TableRow key={row.id}>
+											<TableCell>{row.type}</TableCell>
+											<TableCell>{row.name}</TableCell>
+											<TableCell>{row.balance}</TableCell>
+											<TableCell>
+												<IconButton
+													data-testid={`delete-${row.id}`}
+													onClick={() => {
+														handleDelete(row.id)
+													}}
+												>
+													<DeleteIcon />
+												</IconButton>
+											</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+							{calcLoading ? (
+								<Skeleton />
+							) : (
+								<TableRow>
+									<TableCell>Total</TableCell>
+									<TableCell />
+									<TableCell>{recordSum}</TableCell>
+								</TableRow>
+							)}
+						</>
+					) : (
+						<p>
+							<em>Create a record to get started!</em>
+						</p>
+					)}
+				</>
 			)}
 		</TableContainer>
 	)
